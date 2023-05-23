c86a368c925747e922f47a231e0927d24b5b125c
rokasj <rokas@jerio.me>
2023-05-23 16:03:43 -0400

    Testing test route with fetch

--- practice-for-ch-react-authenticate-me-long-practice-main/authenticate-me/app/controllers/application_controller.rb
+++ practice-for-ch-react-authenticate-me-long-practice-main/authenticate-me/app/controllers/application_controller.rb
@@ -5,6 +5,26 @@ class ApplicationController < ActionController::API
     before_action :snake_case_params
     before_action :attach_authenticity_token
 
+    def current_user
+        @current_user ||= User.find_by(session_token: session[:session_token])
+    end
+
+    def require_logged_in
+        if !logged_in?
+            render json: { errors: ['Must be logged in'], status: :unauthorized}
+        end
+    end
+
+    def login(user)
+        session[:session_token] = user.reset_session_token!
+    end
+
+    def logout
+        current_user.reset_session_token!
+        session[:session_token] = nil
+        @current_user = nil
+    end
+
     private
     def snake_case_params
         params.deep_transform_keys!(&:underscore)
@@ -12,5 +32,19 @@ class ApplicationController < ActionController::API
 
     def attach_authenticity_token
         headers['X-CSRF-Token'] = masked_authenticity_token(session)
+    end
+
+    def test
+        if params.has_key?(:login)
+            login!(User.first)
+        elsif params.has_key?(:logout)
+            logout!
+        end
+
+        if current_user
+            render json: { user: current_user.slice['id', 'username', 'session_token'] }
+        else
+            render json: ['No current user']
+        end
     end
 end
